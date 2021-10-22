322a05ca66671faf669683effe4cb7cd2c834f99
GarimaCisco <92945027+GarimaCisco@users.noreply.github.com>
2021-10-22 23:52:07 +0530

    Delete common_file.py

--- common_file.py
+++
@@ -1,8 +0,0 @@
-with open('input1_diff_coverage_final.summary','r') as file1:
-    with open('input2_diff_coverage_final.summary','r') as file2:
-        same=set(file1).intersection(file2)
-same.discard('\n')
-with open('common.txt','w') as file_out:
-    for line in same:
-        file_out.write(line)
-~                             
